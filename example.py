27e485587f9db2a662e3f1727bd9c070718caabc
Johnson <108435026+Sjhns@users.noreply.github.com>
2025-10-19 15:58:07 -0300

    Create example.py

---
+++ example.py
@@ -0,0 +1,22 @@
+from sudoku_solver import resolver_sudoku, print_sudoku
+
+sudoku = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+]
+
+print("Sudoku inicial:")
+print_sudoku(sudoku)
+
+if resolver_sudoku(sudoku):
+    print("Sudoku resolvido:")
+    print_sudoku(sudoku)
+else:
+    print("Sem solução possível.")
